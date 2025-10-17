d50a6052751627f9bb8fd16f407bbb7020f5726f
academicschoolbd <159193937+academicschoolbd@users.noreply.github.com>
2025-10-17 19:41:45 +0600

    Create validator.js

---
+++ js/validator.js
@@ -0,0 +1,29 @@
+function app_form_validator(theForm)
+{
+	// Board
+	if (theForm.board.selectedIndex <= 0)
+	{
+		alert("Please Select Board!");
+		theForm.board.focus();
+		return (false);
+	}
+	// Roll No
+	if (theForm.roll.value == "")
+	{
+		alert("Please Enter Exam Roll!");
+		theForm.roll.focus();
+		return (false);
+	}
+
+}
+
+// Numbers Only
+function onlyNumbers(evt)
+{
+	var e = event || evt; // for trans-browser compatibility
+	var charCode = e.which || e.keyCode;
+	if (charCode > 31 && (charCode < 47 || charCode > 57))
+	return false;
+	return true;
+}
+;
